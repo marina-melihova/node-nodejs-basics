8df297eb1748e99542bba01a4d1e86194b390913
Marina-Melihova <marinamelihova29@gmail.com>
2022-12-02 20:45:07 +0200

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const { promises: fsPromises } = fs;
+
+const sha256 = content => {
+  return createHash('sha256').update(content).digest('hex');
+};
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const fileBuffer = await fsPromises.readFile(file);
+  console.log(sha256(fileBuffer));
 };
 
 await calculateHash();
