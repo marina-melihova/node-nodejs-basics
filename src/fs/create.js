b7d6249878f029fae0d18ee3a874a6b69c870fea
marina-melihova <melihova.mary@gmail.com>
2022-12-02 12:56:31 +0200

    init: NodeJS basics task-1

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
